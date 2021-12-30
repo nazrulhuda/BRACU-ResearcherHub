9daff8a0a0a0c44f71d488da0e50980ef6a47682
Nazrul Huda Shanto <md.nazrul.huda.shanto@g.bracu.ac.bd>
2021-12-31 00:18:09 +0600

    robot

--- studybud/settings.py
+++ studybud/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-3#av2c6nptlbbb6^muqkchu&fe3wv&n$t2+g$v!ir-f5%doocb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['bracuresearcherhub.herokuapp.com']
+ALLOWED_HOSTS = ['bracuresearcherhub.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
