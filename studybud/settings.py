df35442fc6c68eb0af555adae7c7aa4c7a78f105
Nazrul Huda Shanto <md.nazrul.huda.shanto@g.bracu.ac.bd>
2021-12-31 00:34:12 +0600

    sadasdas

--- studybud/settings.py
+++ studybud/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from pathlib import Path
-import django_heroku
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -150,4 +150,3 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOW_ALL_ORIGINS = True
 
-django_heroku.settings(locals())
