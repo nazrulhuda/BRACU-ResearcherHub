644c1b79786825fa72e086ac555e60f0b16abe5e
Nazrul Huda <nazrulhudashanto@gmail.com>
2022-12-07 23:24:08 +0600

    ddf

--- build.sh
+++ build.sh
@@ -5,8 +5,5 @@ set -o errexit
 
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
-python manage.py shell -c "from django.contrib.auth.models import User; \
-                           User.objects.filter(username='admin1').exists() or \
-                           User.objects.create_superuser('admin1',
-                           'admin1@example.com', 'admin1')"
+
 python manage.py migrate
